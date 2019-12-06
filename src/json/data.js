ac487bf334e56fefe5ac1031b9be02904c40db12
spiritGo <1194501891@qq.com>
2019-12-06 16:02:05 +0800

    first commit

---
+++ src/json/data.js
@@ -0,0 +1,228 @@
+const data = [{
+	title: "懒人图库",
+	url: "http://www.lanrentuku.com/gif/a/mianfei.html"
+}, {
+	title: "animate.css",
+	url: "https://daneden.github.io/animate.css"
+}, {
+	title: "font",
+	url: "https://en.fontke.com/font"
+}, {
+	title: "w3school",
+	url: "http://www.w3school.com.cn/"
+}, {
+	title: "bootstrap中文",
+	url: "http://www.bootcss.com/"
+}, {
+	title: "veer图库",
+	url: "https://www.veer.com/photo/"
+}, {
+	title: "css命名规范",
+	url: "https://wenku.baidu.com/view/76d5cdee52ea551811a687a9.html"
+}, {
+	title: "天翼云网盘",
+	url: "https://cloud.189.cn/"
+}, {
+	title: "pxtoem",
+	url: "http://pxtoem.com/"
+}, {
+	title: "pxtoemconversion",
+	url: "https://www.runoob.com/tags/ref-pxtoemconversion.html"
+}, {
+	title: "icomoon",
+	url: "https://icomoon.io/"
+}, {
+	title: "iconfont",
+	url: "https://www.iconfont.cn/"
+}, {
+	title: "sublime插件",
+	url: "https://blog.csdn.net/jinhui157/article/details/72887142/"
+}, {
+	title: "regexper",
+	url: "https://regexper.com/"
+}, {
+	title: "几款整站抓取的工具",
+	url: "https://blog.csdn.net/junoohoome/article/details/62216023"
+}, {
+	title: "swiper",
+	url: "https://www.swiper.com.cn/"
+}, {
+	title: "github",
+	url: "https://github.com/"
+}, {
+	title: "壁纸",
+	url: "http://image.baidu.com/search/index?tn=baiduimage&ct=201326592&lm=-1&cl=2&ie=gb18030&word=%B1%DA%D6%BD&fr=ala&ala=1&pos=0&alatpl=wallpaper&oriquery=%E5%A3%81%E7%BA%B8"
+}, {
+	title: "vue.js",
+	url: "http://www.runoob.com/vue2/vue-tutorial.html"
+}, {
+	title: "主流框架对比",
+	url: "https://www.cnblogs.com/terrylin/p/4750550.html"
+}, {
+	title: "createjs",
+	url: "http://www.createjs.cc/"
+}, {
+	title: "MDN web docs",
+	url: "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript"
+}, {
+	title: "webpack英文",
+	url: "https://webpack.github.io/"
+}, {
+	title: "webpack中文",
+	url: "https://www.webpackjs.com/"
+}, {
+	title: "fullpage.js",
+	url: "https://alvarotrigo.com/fullPage/#page1"
+}, {
+	title: "vue官网",
+	url: "https://cn.vuejs.org/"
+}, {
+	title: "NPM 学习笔记整理",
+	url: "https://segmentfault.com/p/1210000009653830/read"
+}, {
+	title: "echarts",
+	url: "https://echarts.baidu.com/"
+}, {
+	title: "cocos",
+	url: "https://www.cocos.com/"
+}, {
+	title: "百度地图",
+	url: "http://lbsyun.baidu.com/"
+}, {
+	title: "mint-ui",
+	url: "https://mint-ui.github.io/docs/#/zh-cn2"
+}, {
+	title: "SVG动画JS库",
+	url: "https://baijiahao.baidu.com/s?id=1596694850888883618&wfr=spider&for=pc"
+}, {
+	title: "vsCode",
+	url: "https://marketplace.visualstudio.com"
+}, {
+	title: "[VSCode] 常用插件",
+	url: "https://www.jianshu.com/p/efa53b873d37"
+}, {
+	title: "api工厂",
+	url: "https://www.it120.cc/"
+}, {
+	title: "wxapi",
+	url: "https://github.com/gooking/wxapi"
+}, {
+	title: "weui",
+	url: "https://github.com/Tencent/weui-wxss"
+}, {
+	title: "草料二维码",
+	url: "https://cli.im/"
+}, {
+	title: "ionic",
+	url: "https://ionicframework.com/"
+}, {
+	title: "超级简历",
+	url: "https://www.wondercv.com/"
+}, {
+	title: "element-ui",
+	url: "https://element.eleme.cn/#/zh-CN"
+}, {
+	title: "微信小程序开发文档",
+	url: "https://developers.weixin.qq.com/miniprogram/dev/index.html"
+}, {
+	title: "前端书籍",
+	url: "http://www.linqing07.com/book.html"
+}, {
+	title: "scrollrevealjs",
+	url: "https://scrollrevealjs.org/"
+}, {
+	title: "animejs",
+	url: "http://www.animejs.cc/"
+}, {
+	title: "Hover.css",
+	url: "http://ianlunn.github.io/Hover/"
+}, {
+	title: "vivusjs",
+	url: "https://github.com/maxwellito/vivus#vivusjs"
+}, {
+	title: "JavaScript动画库",
+	url: "http://web.jobbole.com/92301/"
+}, {
+	title: "adobe XD",
+	url: "https://www.adobe.com/cn/products/xd/features.html"
+}, {
+	title: "uniapp",
+	url: "https://uniapp.dcloud.io/"
+}, {
+	title: "webGL",
+	url: "https://www.w3cschool.cn/webgl/9s721ohh.html"
+}, {
+	title: "webhek",
+	url: "http://www.webhek.com/"
+}, {
+	title: "ECMAScript 6简介",
+	url: "http://caibaojian.com/es6/"
+}, {
+	title: "vant",
+	url: "http://npm.taobao.org/package/vant-weapp"
+}, {
+	title: "axios",
+	url: "http://www.axios-js.com/"
+}, {
+	title: "apizza",
+	url: "https://apizza.net/pro/#/"
+}, {
+	title: "flexible",
+	url: "https://github.com/amfe/lib-flexible"
+}, {
+	title: "图片转base64",
+	url: "http://imgbase64.duoshitong.com/"
+}, {
+	title: "图库",
+	url: "https://www.tukuppt.com/"
+}, {
+	title: "图片压缩",
+	url: "https://tinypng.com/"
+}, {
+	title: "tinypng",
+	url: "https://tinypng.com/"
+}, {
+	title: "html5plus",
+	url: "http://www.html5plus.org/doc/zh_cn/barcode.html"
+}, {
+	title: "百度AI",
+	url: "http://ai.baidu.com/docs#/TTS-API/top"
+}, {
+	title: "系统之家",
+	url: "http://xt.halede1.cn/win1064.html"
+}, {
+	title: "微信下调试H5页面",
+	url: "https://blog.csdn.net/weixin_33935777/article/details/88531449"
+}, {
+	title: "Web前端知识体系",
+	url: "https://www.cnblogs.com/onepixel/p/7021506.html"
+}, {
+	title: "字体转base64",
+	url: "https://transfonter.org/"
+}, {
+	title: "浏览器插件开发",
+	url: "https://www.cnblogs.com/liuxianan/p/chrome-plugin-develop.html"
+}, {
+	title: "设计之家",
+	url: "http://www.sj33.cn/digital/uisj/"
+}, {
+	title: "微元素",
+	url: "https://www.element3ds.com/"
+}, {
+	title: "electron",
+	url: "https://electronjs.org/docs/tutorial/first-app#installing-electron"
+}, {
+	title: "Electron使用electron-packager打包",
+	url: "https://www.cnblogs.com/dullfish/p/9300213.html"
+}, {
+	title: "HTML + Javascript 开发桌面应用程序",
+	url: "https://blog.csdn.net/yjwx0018/article/details/52431716"
+}, {
+	title: "vue-cli3使用svg",
+	url: "https://www.jianshu.com/p/1150876dfa04"
+}, {
+	title: "googleMap",
+	url: "https://www.runoob.com/googleapi/googleapi-tutorial.html"
+}]
+
+export default data;
---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="home">
+		<div v-for="(item, index) in data" :key="index" class="margin" @click="goWeb" :data-path="item.url">
+			<el-button :type="index%2 == 0 ? 'primary':'danger'" icon="el-icon-position">{{ item.title }}</el-button>
+		</div>
+	</div>
+</template>
+
+<script>
+	// @ is an alias to /src
+	import data from "../json/data.js"
+	export default {
+		data() {
+			return {
+				data: data
+			}
+		},
+		methods: {
+			goWeb(e) {
+				location.href = e.currentTarget.dataset.path;
+			}
+		}
+	}
+</script>
+<style lang="scss" scoped>
+	.home {
+		background-color: #303133;
+		max-width: 1200px;
+		box-sizing: border-box;
+		margin: 50px auto;
+		padding: 10px;
+		display: flex;
+		flex-wrap: wrap;
+		border-radius: 2px;
+	}
+
+	.margin {
+		margin: 5px;
+		box-shadow: 0 2px 4px #999;
+		border-radius: 6px;
+	}
+</style>
